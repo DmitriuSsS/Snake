5028aca4b1f7ec0862381de78209c2fceb39017d
DmitriuSsS <dmitruish@outlook.com>
2019-11-24 23:39:49 +0300

    Redid the structure of Drawing

--- game/drawing/game_drawing.py
+++ game/drawing/game_drawing.py
@@ -9,9 +9,7 @@ from game.settings import Settings
 
 settings = Settings()
 
-__all__ = ['Button',
-           'Menu', 'WinWindow', 'EndFreeGameWindow', 'GameOverWindow',
-           'GameDrawing', 'FieldDrawing', 'FreeGameDrawing', 'LevelDrawing']
+__all__ = ['GameDrawing', 'FieldDrawing', 'FreeGameDrawing', 'LevelDrawing']
 
 
 def _load_image(name):
@@ -23,131 +21,6 @@ def _draw_image(play_surface, image, left, top, rotate_angle=0):
         image = pygame.transform.rotate(image, rotate_angle)
     rect_image = image.get_rect(topleft=(top, left))
     play_surface.blit(image, rect_image)
-
-
-class Button:
-    def __init__(self,
-                 surface,
-                 x, y,
-                 width, height,
-                 text, text_color=pygame.Color('black'),
-                 button_color=pygame.Color('white'),
-                 href=None,
-                 name=None):
-        self.parent_surface = surface
-        self.x = x
-        self.y = y
-        self.height = height
-        self.width = width
-        self.text = text
-        self.text_color = text_color
-        self.button_color = button_color
-        self.href = href
-        self.name = text if name is None else name
-        self._rect = pygame.Rect(x, y, width, height)
-
-    def draw(self):
-        self._draw_button()
-        self._write_text()
-
-    def _get_size_for_calibri(self):
-        # формула специально для шрифта Calibri в pygame
-        size_width = 8 * self.width // (5 * len(self.text)) - 1
-        size_height = 2 * self.height // 3 - 1
-        return max(min(size_height, size_width), 1)
-
-    def _write_text(self):
-        font_size = self._get_size_for_calibri()
-        font = pygame.font.SysFont("Calibri", font_size)
-        text = font.render(self.text, True, self.text_color)
-        self.parent_surface.blit(text,
-                                 (self.x + (self.width - text.get_width()) / 2,
-                                  self.y + (self.height - text.get_height()) / 2))
-
-    def _draw_button(self):
-        width_circle = min(self.height, self.width) // 20
-        color = self.button_color // pygame.Color(2, 2, 2, 255)
-        color.a = self.button_color.a
-
-        pygame.draw.rect(self.parent_surface, self.button_color,
-                         (self.x, self.y, self.width, self.height))
-
-        # обводка прямоугольника (кнопки)
-        points = [(self.x, self.y),
-                  (self.x, self.y + self.height),
-                  (self.x + self.width, self.y + self.height),
-                  (self.x + self.width, self.y)]
-        for i in range(4):
-            pygame.draw.line(self.parent_surface, color,
-                             points[i], points[i - 3], width_circle)
-
-    def is_pressed(self, mouse):
-        return (self._rect.topleft[0] < mouse[0] < self._rect.bottomright[0]
-                and self._rect.topleft[1] < mouse[1] < self._rect.bottomright[1])
-
-# region windows
-
-
-class Menu:
-    def __init__(self):
-        self.surface = pygame.display.set_mode((200, 160))
-        self.buttons = [Button(self.surface, 20, 20, 160, 50, 'Game with Levels', href='level game'),
-                        Button(self.surface, 20, 90, 160, 50, 'Free game', href='free game')]
-
-    def draw(self):
-        self.surface.fill(pygame.Color('white'))
-        for button in self.buttons:
-            button.draw()
-
-
-class WinWindow:
-    def __init__(self):
-        self.surface = pygame.display.set_mode((200, 160))
-        self.buttons = [Button(self.surface, 20, 100, 160, 40, 'OK',
-                               href='menu')]
-
-    def draw(self):
-        self.surface.fill(pygame.Color('white'))
-        font_win = pygame.font.SysFont("Calibri", 40)
-        text_win = font_win.render('You WIN!', True, pygame.Color('black'))
-        self.surface.blit(text_win, ((self.surface.get_width() - text_win.get_width()) / 2, 30))
-        for button in self.buttons:
-            button.draw()
-
-
-class GameOverWindow:
-    def __init__(self):
-        self.surface = pygame.display.set_mode((200, 160))
-        self.buttons = [Button(self.surface, 20, 100, 160, 40, 'OK', href='menu')]
-
-    def draw(self):
-        self.surface.fill(pygame.Color('white'))
-        font_lose = pygame.font.SysFont("Calibri", 30)
-        text_lose = font_lose.render('Game Over :(', True, pygame.Color('black'))
-        self.surface.blit(text_lose, ((self.surface.get_width() - text_lose.get_width()) / 2, 30))
-        for button in self.buttons:
-            button.draw()
-
-
-class EndFreeGameWindow:
-    def __init__(self, score):
-        self.score = score
-        self.surface = pygame.display.set_mode((200, 160))
-        self.buttons = [Button(self.surface, 20, 100, 160, 40, 'OK', href='menu')]
-
-    def draw(self):
-        self.surface.fill(pygame.Color('white'))
-        font_win = pygame.font.SysFont("Calibri", 25)
-        text_win = font_win.render('End of the Game', True, pygame.Color('black'))
-        self.surface.blit(text_win, ((self.surface.get_width() - text_win.get_width()) / 2, 20))
-
-        font_score = pygame.font.SysFont("Calibri", 20)
-        text_score = font_score.render(f'Score: {self.score}', True, pygame.Color('black'))
-        self.surface.blit(text_score, ((self.surface.get_width() - text_score.get_width()) / 2, 50))
-        for button in self.buttons:
-            button.draw()
-
-# endregion
 
 
 class FieldDrawing:
--- game/game.py
+++ game/game.py
@@ -2,7 +2,8 @@ import random
 
 from game.entities import Food, Level
 from game.direction import *
-from game.drawing import *
+from game.drawing.windows import *
+from game.drawing.game_drawing import *
 from game.settings import Settings
 import pygame
 pygame.init()
